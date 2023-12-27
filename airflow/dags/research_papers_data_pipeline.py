2ac20ce4590fc232f2adb76a583cf3a18b5d8688
David Avedis Injarabian <david.avedis.inj@gmail.com>
2023-12-27 03:05:54 +0200

    Updated compose, added dags, changed file names. Does not currently work, dependencies need to be installed before initializing airflow services

---
+++ airflow/dags/research_papers_data_pipeline.py
@@ -0,0 +1,71 @@
+import os
+import zipfile
+import pandas as pd
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def download_dataset():
+    dataset_path = '/tmp/airflow/dataset/arxiv.zip'
+
+    # Check if the dataset already exists
+    if not os.path.exists(dataset_path):
+        api = KaggleApi()
+        api.authenticate()
+        api.dataset_download_files('Cornell-University/arxiv', path='/tmp/airflow/dataset/', unzip=False)
+
+def unzip_dataset():
+    dataset_path = '/tmp/airflow/dataset/arxiv.zip'
+    extracted_path = '/tmp/airflow/dataset/arxiv-metadata-oai-snapshot.json'
+
+    # Check if the dataset zip file exists and if the extracted file does not exist
+    if os.path.exists(dataset_path) and not os.path.exists(extracted_path):
+        with zipfile.ZipFile(dataset_path, 'r') as zip_ref:
+            zip_ref.extractall('/tmp/airflow/dataset/')
+
+def transform_and_save():
+    file_path = '/tmp/airflow/dataset/arxiv-metadata-oai-snapshot.json'
+    output_path = '/tmp/airflow/staging_area/arxiv_transformed.csv'
+
+    if os.path.exists(file_path):
+        df = pd.read_json(file_path, lines=True)
+        df = df.dropna(subset=['doi'])
+        df.to_csv(output_path, index=False)
+    else:
+        print(f"File {file_path} does not exist. Transformation skipped.")
+
+with DAG(
+    'download_transform_arxiv_data',
+    default_args=default_args,
+    description='DAG to download, transform and save arxiv dataset',
+    schedule_interval=timedelta(days=1),
+    catchup=False
+) as dag:
+
+    t1 = PythonOperator(
+        task_id='download_dataset',
+        python_callable=download_dataset,
+    )
+
+    t2 = PythonOperator(
+        task_id='unzip_dataset',
+        python_callable=unzip_dataset,
+    )
+
+    t3 = PythonOperator(
+        task_id='transform_and_save',
+        python_callable=transform_and_save,
+    )
+
+    t1 >> t2 >> t3
